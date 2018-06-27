97cfa3d480934282942f5273acb061a98cbb0068
Sagar Ladla <sagarladla10@gmail.com>
2018-06-27 21:07:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,47 +6,40 @@ def hypothesis(b,m,x):
 
 def cost(b,m,points):
 	totalError = 0
-	M = float(len(points))
+	M = (float(len(points)))
 	for i in range(len(points)):
-		x = points[i,0]
-		y = points[i,1]
-		totalError += (hypothesis(b,m,x) - y)**2
-	return totalError / (2*M)
+		x = float(points[i,0])
+		y = float(points[i,1])
+		totalError += (1/(2*M)) * ((hypothesis(b,m,x) - y)**2)
+	return totalError
 
-def gradient_descent(b,m,points,alpha):
+def gradient_descent(b,m,points,alpha,epochs):
 	partial_b=partial_m=0
-	for i in range(len(points)):
-		x = points[i,0]
-		y = points[i,1]
-		partial_b += (hypothesis(b,m,x) - y)
-		partial_m += (hypothesis(b,m,x) - y) * x
-	
-	partial_b /= float(len(points))
-	partial_m /= float(len(points))
-	
-	new_b = (b - (alpha*partial_b))
-	new_m = (m - (alpha*partial_m))
-	
-	return [new_b, new_m]
-
-def linear_regression(b=0,m=0,points=None,alpha=0.00005,epochs=1000):
 	new_b = b
 	new_m = m
-	for i in range(epochs):
-		new_b, new_m = gradient_descent(b,m,points,alpha)
+	for convergence in range(epochs):
+		for i in range(len(points)):
+			x = float(points[i,0])
+			y = float(points[i,1])
+			partial_b += (1/float(len(points))) * (hypothesis(new_b,new_m,x) - y)
+			partial_m += (1/float(len(points))) * (hypothesis(new_b,new_m,x) - y) * x
+		new_b = (b - (alpha*partial_b))
+		new_m = (m - (alpha*partial_m))
 	return [new_b, new_m]
 
 def main():
+	b=m=0
+	alpha = 0.0008
+	epochs = 1000
 	points = np.genfromtxt('data.csv', delimiter=',')
-	new_b, new_m = linear_regression(b=0, m=0, points=np.array(points), alpha=0.0004, epochs=1000)
-	x=int(input("Enter X value to predict : "))
+	[new_b, new_m] = gradient_descent(b, m, np.array(points), alpha, epochs)
+	x = float(input("Enter X value to predict : "))
 	y = hypothesis(new_b,new_m,x)
 	print('Predicted price is : {}'.format(y))
 
 	print("Cost function's weights are b:{}, m:{}".format(new_b, new_m))
-	print("Total cost is : {}".format(cost(new_b,new_m,np.array(points))))
+	print("Total cost is : {}".format(cost(new_b,new_m,np.asarray(points))))
 	
 if __name__ == '__main__':
 	main()
-	
 	
